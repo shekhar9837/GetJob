32f8e9f0740a6b86cfad1bb7aba00f3979605a04
Chandra Shekhar <mauryashekhar9837@gmail.com>
2024-08-06 22:56:47 +0530

    env

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-
 const corsOptions = {
-    origin:process.env.BACKEND_URI,
+    origin:'http://localhost:5173',
     credentials:true
 }
 
