30074f1cea33b96ae82903cfbe7605ffaf913f7b
Chandra Shekhar <mauryashekhar9837@gmail.com>
2024-08-07 15:39:04 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,6 @@ import PostJob from './components/admin/PostJob'
 import Applicants from './components/admin/Applicants'
 import ProtectedRoute from './components/admin/ProtectedRoute'
 
-
 const appRouter = createBrowserRouter([
   {
     path: '/',
